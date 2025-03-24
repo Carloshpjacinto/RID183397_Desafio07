ca342f1fffd763d4d7ff1ce2bd991c9fbd801adb
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-03-24 00:08:28 -0300

    feature/correcao-auth-guard

--- src/shared/guards/Auth.guard.ts
+++ src/shared/guards/Auth.guard.ts
@@ -8,6 +8,7 @@ import { JwtService } from '@nestjs/jwt';
 import { jwtSecretConstants } from 'src/modules/auth/constants/jwtSecret.constants';
 import { Request } from 'express';
 
+
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
